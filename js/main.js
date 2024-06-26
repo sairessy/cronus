c084b1af85d106b54ae7fc980ce2e0b1a1c3b31c
sairessy <sairessy@gmail.com>
2024-06-25 20:25:25 -0700

    Tiny fixes

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ document.querySelector("body").ondblclick = (e) => {
 
   recognition.onresult = (e) => {
     text = e.results[0][0].transcript;
-    document.getElementById("text").innerHTML = text;
     getIntent(text);
   };
   
