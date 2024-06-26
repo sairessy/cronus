7b705f3feee430f2d2d01771f9eed39ae55bd8a9
sairessy <sairessy@gmail.com>
2024-06-25 18:39:37 -0700

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,51 +1,44 @@
 let listening = false;
 let text = "";
 
-let recognition;
+wakeupAPI();
+const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+recognition.interimResults = false;
+recognition.continuous = false;
 
-window.addEventListener("load", (e) => {
-  console.log("Page loaded.");
-  wakeupAPI();
-  recognition = new webkitSpeechRecognition() || new SpeechRecognition();
-  recognition.interimResults = false;
-  recognition.continuous = false;
+
+document.querySelector("body").ondblclick = (e) => {
+  document.querySelector(".logo h1").style.color = colors.primary;
+
+  listening = false;
 
   recognition.start();
-  
-  document.querySelector('.logo h1').style.color = '#aaa';
 
   recognition.onresult = (e) => {
     text = e.results[0][0].transcript;
     getIntent(text);
   };
-
+  
   recognition.onstart = () => {
     console.log("Reconhecimento de voz iniciado...");
   };
-
+  
   recognition.onend = () => {
     console.log("Ended trying to recognize");
     document.querySelector(".logo h1").style.color = "#aaa";
   };
-
+  
   recognition.onerror = (err) => {
     console.log(err);
   };
+  
+};
 
-  document.querySelector("body").ondblclick = (e) => {
-    document.querySelector(".logo h1").style.color = colors.primary;
-  
-    listening = false;
-  
-    recognition.start();
-  };
-  
-  document.getElementById("form-chat").addEventListener("submit", (e) => {
-    e.preventDefault();
-    const text = document.getElementById("inp-text").value;
-    getIntent(text);
-    document.getElementById("inp-text").value = "";
-  });
+document.getElementById("form-chat").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = document.getElementById("inp-text").value;
+  getIntent(text);
+  document.getElementById("inp-text").value = "";
 });
 
 async function getIntent(text) {
